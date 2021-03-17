9145f173ec89f6acb1b4fcdbc92f56989d2121e4
fsifuentes <fsifuentes@SISTEMAS15>
2021-03-16 19:55:21 -0500

    barra menu operativa en otras ventanas

--- javascript/menu.js
+++ javascript/menu.js
@@ -3,21 +3,21 @@ $(document).ready(main);
 var contador = 1;
 
 function main() {
-$('.menu_bar').click(function(){
-//$('nav').toggle();
-if(contador == 1){
-    $('nav').animate({
-        left:'0'
-    });
-    contador = 0;
-}else{
-    contador = 1;
-    $('nav').animate({
-        left:'-100%'
-    });
-
-}
-});}
+                  $('.menu_bar').click(function()
+                  //$('nav').toggle();
+                  {                  
+                        if(contador == 1)
+                           {
+                              $('nav').animate({ left:'0' });
+                              contador = 0;
+                           }
+                      else{
+                              contador = 1;
+                              $('nav').animate({ left:'-100%'});
+                          }
+                  }
+                  );
+               }
 
 
 
@@ -28,29 +28,24 @@ window.onscroll = function()
     let Desplazamiento_actual = window.pageYOffset; 
     if(ubicacionPrincipal >= Desplazamiento_actual)
     {
-        document.getElementById('navbar').style.top = '0';
+        document.getElementById('navbar').style.left = '-100%';
+      //  contador = 0;
     }
-    else
-     {
-        document.getElementById('navbar').style.top = '100%';
-     }
-
-     if(ubicacionPrincipal <= Desplazamiento_actual)
-     {
-         document.getElementById('navbar').style.top = '100%';
-     }
      else
       {
-         document.getElementById('navbar').style.top = '0';
+         document.getElementById('navbar').style.left = '100%';
+         contador = 1;
       }
-    //if(ubicacionPrincipal <= Desplazamiento_actual)
-   // {
-   //     document.getElementById('navbar').style.top = '-100%';
-   // }
-    // else
-    // {
-    //     document.getElementById('navbar').style.top = '100%';
-    // }
+
+    //  if(ubicacionPrincipal <= Desplazamiento_actual)
+    //  {
+    //      document.getElementById('navbar').style.top = '100%';
+    //  }
+    //  else
+    //   {
+    //      document.getElementById('navbar').style.top = '0';
+    //   }
+  
   
     ubicacionPrincipal = Desplazamiento_actual;
 }
