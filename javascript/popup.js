bff1a9f6c1862bcd58ba52f5af24fcd905ca1b9b
Frank S. M <frankluis158@gmail.com>
2021-03-27 10:23:16 -0500

    mejora de video para ventana modal

--- javascript/popup.js
+++ javascript/popup.js
@@ -5,48 +5,29 @@ btnCerrarPopup = document.getElementById('btn-cerrar-popup'),
 desaparecer = document.getElementById('desaparecer'),
 src = document.getElementById('src'),
 h3 = document.getElementById('h3'),
-h4 = document.getElementById('h4');
-video = document.getElementById('video');
+h4 = document.getElementById('h4'),
+video = document.getElementById('video'),
 aparecer = document.getElementById('aparecer');
 
-
+// newElement.textContent = "sdfsd";
 
 btnAbrirPopup.addEventListener('click',function(){
     overlay.classList.add('active');
     popup.classList.add('active');
-    h3.classList.add('active');
-   // video.add('active');
-   
-    
-    // video.classList.add('video');
-    // vi.classList.add('active');
+    //  h5.after(video.cloneNode(true));
+  
 });
 
 
 btnCerrarPopup.addEventListener('click',function(){
     overlay.classList.remove('active');
     popup.classList.remove('active');
+    document.getElementById('video').replaceWith(video);
     
-    // video.classList.remove('video');
-    // video.classList.removeAttr('active');
-  //  video.pause();
-  //  video.currentTime = 0;
-    
-    // enlace.classList.removeAttr('src');
-
+    // video.remove();
+ 
 });
 
-desaparecer.addEventListener('click',function(){  
-    document.getElementById('h3').style.display = 'none';
-    document.getElementById('video').style.display = 'none';
-    document.getElementById('video');
-});
-
-aparecer.addEventListener('click',function(){
-    document.getElementById('h3').style.display = 'block';
-    document.getElementById('video').style.display = 'block';
-    document.getElementById('video');
-});
 // desaparecer.addEventListener('click',function(){  
 //    h3.remove();   
 // });
@@ -74,22 +55,22 @@ aparecer.addEventListener('click',function(){
 //////////////////////////////////2//////////////////////////////////////////
 
 
-// var btnAbrirPopup2 = document.getElementById('btn-abrir-popup2'),
-// overlay2 = document.getElementById('overlay2'),
-// popup2 = document.getElementById('popup2'),
-// btnCerrarPopup2 = document.getElementById('btn-cerrar-popup2');
+var btnAbrirPopup_2 = document.getElementById('btn-abrir-popup_2'),
+overlay_2 = document.getElementById('overlay_2'),
+popup_2 = document.getElementById('popup_2'),
+btnCerrarPopup_2 = document.getElementById('btn-cerrar-popup_2');
 
 
 
-// btnAbrirPopup2.addEventListener('click',function(){
-//     overlay2.classList.add('active');
-//     popup2.classList.add('active');
-// });
+btnAbrirPopup_2.addEventListener('click',function(){
+    overlay_2.classList.add('active');
+    popup_2.classList.add('active');
+});
 
-// btnCerrarPopup2.addEventListener('click',function(){
-//     overlay2.classList.remove('active');
-//     popup2.classList.remove('active');
-// });
+btnCerrarPopup_2.addEventListener('click',function(){
+    overlay_2.classList.remove('active');
+    popup_2.classList.remove('active');
+});
 
 
 
