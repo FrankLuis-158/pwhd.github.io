f71893f4ce3360cb06ec64da24d0bd7d7489d851
fsifuentes <fsifuentes@SISTEMAS15>
2021-04-09 18:03:09 -0500

    Se agrego el cubo a todos los bloques en el index principal

---
+++ javascript/jquery_cubo_giratorio.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+    $('.more').click(function(){
+    $('.back').addClass('active')
+    $('.front').removeClass('active')
+})
+$('.go-back').click(function(){
+    $('.back').removeClass('active')
+    $('.front').addClass('active')
+})
+})
+
+
+
+$(document).ready(function(){
+    $('.more_2').click(function(){
+    $('.back_2').addClass('active')
+    $('.front_2').removeClass('active')
+})
+$('.go-back_2').click(function(){
+    $('.back_2').removeClass('active')
+    $('.front_2').addClass('active')
+})
+})
+
+
+$(document).ready(function(){
+    $('.more_3').click(function(){
+    $('.back_3').addClass('active')
+    $('.front_3').removeClass('active')
+})
+$('.go-back_3').click(function(){
+    $('.back_3').removeClass('active')
+    $('.front_3').addClass('active')
+})
+})
+
+
+
+$(document).ready(function(){
+    $('.more_4').click(function(){
+    $('.back_4').addClass('active')
+    $('.front_4').removeClass('active')
+})
+$('.go-back_4').click(function(){
+    $('.back_4').removeClass('active')
+    $('.front_4').addClass('active')
+})
+})
+
+
+
+
+$(document).ready(function(){
+    $('.more_5').click(function(){
+    $('.back_5').addClass('active')
+    $('.front_5').removeClass('active')
+})
+$('.go-back_5').click(function(){
+    $('.back_5').removeClass('active')
+    $('.front_5').addClass('active')
+})
+})
+
+
+$(document).ready(function(){
+    $('.more_6').click(function(){
+    $('.back_6').addClass('active')
+    $('.front_6').removeClass('active')
+})
+$('.go-back_6').click(function(){
+    $('.back_6').removeClass('active')
+    $('.front_6').addClass('active')
+})
+})
